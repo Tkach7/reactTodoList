c198625f8da2e4dbfdd11f30f581cf82ed87b658
Ткач Даниил <Tkach@MacBook-Air-Tkac.local>
2017-01-24 17:00:41 +0500

    add update feature

--- src/js/components/todo/todo.jsx
+++ src/js/components/todo/todo.jsx
@@ -9,12 +9,16 @@ export default class TodoList extends React.Component {
             todoItems: [],
             newTodoValue: {
                 value: ''
-            }
+            },
+            editItems: []
         };
         // init handle fucntion
         this.handleChange = this.handleChange.bind(this);
         this.nahdleAdd = this.nahdleAdd.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
+        this.handleCurrentValue = this.handleCurrentValue.bind(this);
+        this.handleChangeEditStatus = this.handleChangeEditStatus.bind(this);
+        this.handleCheckStatusEdit = this.handleCheckStatusEdit.bind(this);
     }
 
     // add new todo
@@ -33,14 +37,49 @@ export default class TodoList extends React.Component {
         this.setState({todoItems: newListItems});
     }
 
+    // edit current value
+    handleCurrentValue(event, key) {
+        let editedItem = this.state.todoItems;
+        editedItem[key].value = event.target.value;
+        this.setState({todoItems: editedItem});
+    }
+
+    // change edit statut
+    handleChangeEditStatus(isEdit, key) {
+        let newList = this.state.editItems;
+
+        if (isEdit) {
+            newList.push(key);
+        } else {
+            newList = newList.filter(function(item) {
+                return item != key;
+            });
+        }
+
+        this.setState({editItems: newList});
+    }
+
+    handleCheckStatusEdit(key) {
+
+        return this.state.editItems.indexOf(key) >= 0;
+    }
+
     render() {
         // generete todo list items
-        let _this = this;
-        let todoList = this.state.todoItems.map(function(item, key) {
+        let todoList = this.state.todoItems.map((item, key) => {
             return (
-                <section key={key} className="todo__item">
-                    <div>{item.value}</div>
-                    <div className="icon-delete" onClick={() => {_this.handleDelete(key)}}></div>
+                <section key={key} className={"todo__item " + (this.handleCheckStatusEdit(key) ?  "item--edit-mode" : "")}>
+                    <input className={"todo__item__value " + (this.handleCheckStatusEdit(key)  ?  "" : "item__change--not-active")}
+                    value={item.value} onChange={(e) => this.handleCurrentValue(e, key)} readOnly/>
+                    <section className="todo__change">
+                        <div
+                        className={"icon icon-edit " + (this.handleCheckStatusEdit(key)  ?  "edit--not-active" : "")}
+                        onClick={() => this.handleChangeEditStatus(true, key)}></div>
+                        <div
+                        className={"icon icon-save " + (this.handleCheckStatusEdit(key) ?  "" : "save--not-active")}
+                        onClick={() => this.handleChangeEditStatus(false, key)}></div>
+                        <div className="icon icon-delete icon--indent" onClick={() => this.handleDelete(key)}></div>
+                    </section>
                 </section>
                 );
         });
